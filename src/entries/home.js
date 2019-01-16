2aeff3c808ebf828ab2daec60e2cea421f25ec8e
Christian Torres <christiantorres084@gmail.com>
2019-01-16 15:24:38 -0600

    se crea index.html que hace direccionamiento al contenido del entrie home.js

---
+++ src/entries/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import Home from './../pages/containers/home';
+
+const homeContainer = document.getElementById('container');
+
+render(<h1>HOME SARE</h1>, homeContainer);
